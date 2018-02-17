7859376248c5b3390dec252e931f546e85c7eb39
Ralf Th. Pietsch <ratopi@abwesend.de>
2018-02-17 22:54:56 +0100

    Using Output/InputStream in CNNLoader

--- src/main/java/javacnn/cnn/CNNLoader.java
+++ src/main/java/javacnn/cnn/CNNLoader.java
@@ -3,29 +3,30 @@ package javacnn.cnn;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
 public class CNNLoader {
 
 	public static void saveModel(final String fileName, final CNN cnn) throws IOException {
 		saveModel(cnn, new FileOutputStream(fileName));
-		return;
 	}
 
-	public static void saveModel(final CNN cnn, final FileOutputStream fileOutputStream) throws IOException {
-		ObjectOutputStream oos = new ObjectOutputStream(fileOutputStream);
+	public static void saveModel(final CNN cnn, final OutputStream outputStream) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(outputStream);
 		oos.writeObject(cnn);
 		oos.flush();
 		oos.close();
 	}
 
-	public static CNN loadModel(String fileName) throws IOException, ClassNotFoundException {
+	public static CNN loadModel(final String fileName) throws IOException, ClassNotFoundException {
 		return loadModel(new FileInputStream(fileName));
 	}
 
-	public static CNN loadModel(final FileInputStream fileInputStream) throws IOException, ClassNotFoundException {
-		final ObjectInputStream in = new ObjectInputStream(fileInputStream);
+	public static CNN loadModel(final InputStream inputStream) throws IOException, ClassNotFoundException {
+		final ObjectInputStream in = new ObjectInputStream(inputStream);
 		final CNN cnn = (CNN) in.readObject();
 		in.close();
 
